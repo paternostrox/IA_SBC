94684763f354bcc77b2a969868d32629a592f38e
Paternostro <pedro.paternostro@gmail.com>
2022-03-17 23:21:17 -0300

    Agora foi (mesmo)

--- test.py
+++
@@ -1,9 +0,0 @@
-def make_question(text):
-    while True:
-        ans = input('\n' + text + 'vsf').to
-        if(ans == 'S' or ans == 'Y'):
-            return True
-        if(anas == 'N'):
-            return True
-
-make_question('vatomanocu?')
